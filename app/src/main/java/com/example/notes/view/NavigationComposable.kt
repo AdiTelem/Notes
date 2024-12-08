192ec3d55cb59db26bf317aa291521c65563eee4
Adi-Telem <adi.telem@autodesk.com>
2024-12-08 11:27:23 +0200

    Fully functioning remote db

--- app/src/main/java/com/example/notes/view/NavigationComposable.kt
+++ app/src/main/java/com/example/notes/view/NavigationComposable.kt
@@ -6,15 +6,17 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.notes.viewmodel.EditScreenViewModel
+import com.example.notes.viewmodel.NavigationViewModel
 import com.example.notes.viewmodel.NoteGalleryViewModel
 
 @Composable
 fun NotesAppNavigation() {
     val navController = rememberNavController()
+    var nVM = NavigationViewModel(LocalContext.current)
 
     NavHost(navController = navController, startDestination = "noteGallery") {
         composable("noteGallery") {
-            val notesListViewModel = NoteGalleryViewModel(navController, LocalContext.current) // Retrieve ViewModel
+            val notesListViewModel = NoteGalleryViewModel(navController, nVM.notes, LocalContext.current) // Retrieve ViewModel
             NoteGallery(notesListViewModel)
         }
 
@@ -22,7 +24,7 @@ fun NotesAppNavigation() {
             val noteID = backStackEntry.arguments?.getString("noteID")?.toIntOrNull()
 
             if (noteID != null) {
-                val editScreenViewModel = EditScreenViewModel(navController, noteID, LocalContext.current)
+                val editScreenViewModel = EditScreenViewModel(navController, nVM.notes, noteID)
                 EditScreen(editScreenViewModel)
             } else {
                 throw IllegalStateException("note to edit not found")
